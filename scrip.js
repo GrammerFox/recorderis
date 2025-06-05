11537aef35c3849d8e879e9fb1ed0430a5a9250a
al3ztm <alexbm2hd@gmail.com>
2025-06-05 18:58:43 -0500

    primer cambio

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const nombre=document.getElementById("name");
+const apellido=document.getElementById("surname");
+
+function enviarFormulario() {
+    alert ("nombre: "+nombre.value+"\napellido: "+ apellido.value)
+}
