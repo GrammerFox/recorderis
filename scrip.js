0c4e6f6652ba102d1d5fb19cdf4592675f273ffe
al3ztm <alexbm2hd@gmail.com>
2025-06-05 19:19:03 -0500

    lo arregle todo chaval

--- scrip.js
+++ scrip.js
@@ -1,6 +1,7 @@
 const nombre=document.getElementById("name");
 const apellido=document.getElementById("surname");
 
+
 function enviarFormulario() {
     alert ("nombre: "+nombre.value+"\napellido: "+ apellido.value)
 }
