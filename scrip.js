082ffe332fd1e0e3f066470c2e7c8852b8a4ffec
al3ztm <alexbm2hd@gmail.com>
2025-06-05 19:52:30 -0500

    mano estas jodido con esto

--- scrip.js
+++ scrip.js
@@ -1,7 +1,11 @@
 const nombre=document.getElementById("name");
 const apellido=document.getElementById("surname");
 
+const nombreResultado = document.getElementById("respuestaNombre")
+const apellidoResultado = document.getElementById("respuestaApellido")
 
-function enviarFormulario() {
-    alert ("nombre: "+nombre.value+"\napellido: "+ apellido.value)
+function enviarFormulario(){
+    nombreResultado.innerText = "nombre:"+nombre.value;
+    apellidoResultado.innerText = "apellido:"+apellido.value;
 }
+
