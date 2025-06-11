d18b52cd0c47f450cc7336da286cce0998e6fbc5
Ernesto56734856 <provisionalernesto@gmail.com>
2025-06-10 19:52:17 -0500

    ya entendimos chamo

--- scrip.js
+++ scrip.js
@@ -1,11 +1,23 @@
-const nombre=document.getElementById("name");
-const apellido=document.getElementById("surname");
+const nombre = document.getElementById('name');
+const apellido = document.getElementById('surname');
+const edad = document.getElementById('age');
 
-const nombreResultado = document.getElementById("respuestaNombre")
-const apellidoResultado = document.getElementById("respuestaApellido")
+const respuestas = document.getElementById ("respuestas")
+const nombreResultado = document.getElementById('respuestaNombre');
+const apellidoResultado = document.getElementById('respuestaApellido');
+const edadResultado = document.getElementById('respuestaEdad');
 
-function enviarFormulario(){
-    nombreResultado.innerText = "nombre:"+nombre.value;
-    apellidoResultado.innerText = "apellido:"+apellido.value;
+function enviarFormulario() {
+  nombreResultado.innerText = 'nombre:' + nombre.value;
+  apellidoResultado.innerText = 'apellido:' + apellido.value;
+  edadResultado.innerText = 'edad:' + edad.value;
+  const edadNumero = parseInt(edad.value);
+
+  if (edadNumero > 18) {
+    edadResultado.innerText = "es mayor de edad"
+    respuestas.classList.remove ("menor")
+  } else {
+    edadResultado.innerText = "es menor de edad"
+    respuestas.classList.add("menor")
+  }
 }
-
