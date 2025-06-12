1417f614946b809b61d84d9d3c81d70e662e8f52
Ernesto56734856 <provisionalernesto@gmail.com>
2025-06-11 19:36:49 -0500

    hermano ya basta porfavor

--- scrip.js
+++ scrip.js
@@ -2,22 +2,30 @@ const nombre = document.getElementById('name');
 const apellido = document.getElementById('surname');
 const edad = document.getElementById('age');
 
-const respuestas = document.getElementById ("respuestas")
+const respuestas = document.getElementById('respuestas');
 const nombreResultado = document.getElementById('respuestaNombre');
 const apellidoResultado = document.getElementById('respuestaApellido');
 const edadResultado = document.getElementById('respuestaEdad');
 
 function enviarFormulario() {
-  nombreResultado.innerText = 'nombre:' + nombre.value;
-  apellidoResultado.innerText = 'apellido:' + apellido.value;
-  edadResultado.innerText = 'edad:' + edad.value;
-  const edadNumero = parseInt(edad.value);
-
-  if (edadNumero > 18) {
-    edadResultado.innerText = "es mayor de edad"
-    respuestas.classList.remove ("menor")
+  if (nombre.value.trim() == '' || apellido.value.trim() == "" ) {
+    alert('teni que poner el nombre chamo');
   } else {
-    edadResultado.innerText = "es menor de edad"
-    respuestas.classList.add("menor")
+    nombreResultado.innerText = 'nombre:' + nombre.value;
+    apellidoResultado.innerText = 'apellido:' + apellido.value;
+    edadResultado.innerText = 'edad:' + edad.value;
+    let edadNumero = parseInt(edad.value);
+
+    if (edadNumero > 18) {
+      edadResultado.innerText = 'es mayor de edad';
+      respuestas.classList.remove('menor');
+    } else {
+      edadResultado.innerText = 'es menor de edad';
+      respuestas.classList.add('menor');
+    }
   }
+}
+
+function temaoscuro(){
+  document.body.classList.toggle("oscuro")
 }
